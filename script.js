6042b280d7d163be3a04d19b3abe7de27cb62050
rkier <rkiernan95@gmail.com>
2025-01-15 16:21:59 -0500

    add html boilerplate

---
+++ script.js
@@ -0,0 +1,49 @@
+console.log("hello world")
+
+
+
+
+let computerChoice
+
+function getComputerChoice() {
+    let rando = Math.random();
+    if (rando > .33 && rando < .66) {
+     computerChoice = "rock" 
+} else if (rando < .33) {
+    computerChoice = "paper"
+} else if (rando > .66) {
+    computerChoice = "scissors" 
+}
+return computerChoice;
+}
+
+
+let humanChoice = prompt("rock paper scissors")
+let result
+
+function playRound() {
+
+if (computerChoice === "rock" && humanChoice === "paper" ) {
+    result = "win"
+} else if (computerChoice === "rock" && humanChoice === "scissors") {
+    result = "loss"
+} else if (computerChoice=== "paper" && humanChoice === "rock" ) {
+    result = "loss"
+} else if (computerChoice === "paper" && humanChoice === "scissors") {
+    result = "win"
+} else if (computerChoice === "scissors" && humanChoice === "paper" ) {
+    result = "loss"
+} else if (computerChoice === "scissors" && humanChoice === "rock") {
+    result = "win"  
+} else if (computerChoice === humanChoice) {
+    result = "tie"} 
+
+return result;
+
+}
+
+
+console.log(humanChoice)
+console.log(getComputerChoice())
+console.log(playRound())
+
